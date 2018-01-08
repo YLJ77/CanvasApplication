b0d80cec0b5f0997985379a8b636389649f38f02
ylj <564296557@qq.com>
2018-01-08 15:06:56 +0800

    添加渐变效果

---
+++ radial-gradients.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    gradient = context.createRadialGradient(
+                canvas.width/2, canvas.height, 10,
+                canvas.width/2, 0, 100);
+
+gradient.addColorStop(0, 'blue');
+gradient.addColorStop(0.25, 'white');
+gradient.addColorStop(0.5, 'purple');
+gradient.addColorStop(0.75, 'red');
+gradient.addColorStop(1, 'yellow');
+
+context.fillStyle = gradient;
+context.rect(0, 0, canvas.width, canvas.height);
+context.fill();
