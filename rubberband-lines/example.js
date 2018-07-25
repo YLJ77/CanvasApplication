969c689182fa45fbf11912e260d1fd1e079f5f0d
YLJ77 <564296557@qq.com>
2018-07-25 08:48:36 +0800

    添加拖动功能

--- rubberband-lines/example.js
+++ rubberband-lines/example.js
@@ -4,6 +4,7 @@ let canvas = document.getElementById('canvas'),
     strokeStyleSelect = document.getElementById('strokeStyleSelect'),
     guidewireCheckbox = document.getElementById('guidewireCheckbox'),
     fillCheckbox = document.getElementById('fillCheckbox'),
+    editCheckbox = document.getElementById('editCheckbox'), 
     // 多边形 start
     sidesSelect = document.getElementById('sidesNumber'),
     startAngleSelect = document.getElementById('startAngle'),
@@ -16,6 +17,11 @@ let canvas = document.getElementById('canvas'),
     mousedown = {},
     rubberbandRect = {},
     dragging = false,
+    draggingOffsetX,
+    draggingOffsetY,
+
+    editing = false,
+    polygons = [],
     guidewires = guidewireCheckbox.checked;
 // Functions..........................................................
 function drawGrid(color, stepx, stepy) {
@@ -88,64 +94,122 @@ function drawRubberbandShape(loc) {
     // 画圆
     // drawCircle(loc);
     // 画多边形
-    drawPolygon(loc);
+    drawPolygon();
 }
 function updateRubberband(loc) {
     updateRubberbandRectangle(loc);
     drawRubberbandShape(loc);
 }
 // Polygon.........................................................
-function getPolygonPoints(centerX, centerY, radius, sides, startAngle) {
-    var points = [],
-        angle = startAngle || 0;
-    for (var i=0; i < sides; ++i) {
-        /*
-        points.push(new Point(centerX + radius * Math.sin(angle),
-            centerY - radius * Math.cos(angle)));
-            */
-
-        points.push(
-            new Point(
-                centerX + radius * Math.cos(angle),
-                centerY - radius * Math.sin(angle)
-            )
-        );
-
-        angle += 2*Math.PI/sides;
-    }
-    return points;
-}
-function createPolygonPath(centerX, centerY, radius, sides, startAngle) {
-    var points = getPolygonPoints(
+class Polygon {
+    constructor(
         centerX,
         centerY,
         radius,
         sides,
-        startAngle 
-    );
-    context.beginPath();
-    context.moveTo(points[0].x, points[0].y);
-    for (var i=1; i < sides; ++i) {
-        context.lineTo(points[i].x, points[i].y);
+        startAngle,
+        strokeStyle,
+        fillStyle,
+        filled 
+    ) {
+        this.x = centerX;
+        this.y = centerY;
+        this.radius = radius;
+        this.sides = sides;
+        this.startAngle = startAngle;
+        this.strokeStyle = strokeStyle;
+        this.fillStyle = fillStyle;
+        this.filled = filled; 
     }
-    context.closePath();
+    getPoints() {
+        var points = [],
+            angle = this.startAngle || 0;
+        for (var i=0; i < this.sides; ++i) {
+            points.push(new Point(this.x + this.radius * Math.sin(angle),
+                this.y - this.radius * Math.cos(angle)));
+            angle += 2*Math.PI/this.sides;
+        }
+        return points;
+    }
+    createPath(context) {
+        var points = this.getPoints();
+        context.beginPath();
+        context.moveTo(points[0].x, points[0].y);
+        for (var i=1; i < this.sides; ++i) {
+            context.lineTo(points[i].x, points[i].y);
+        }
+        context.closePath();
+    }
+    stroke(context) {
+        context.save();
+        this.createPath(context);
+        context.strokeStyle = this.strokeStyle;
+        context.stroke();
+        context.restore();
+    }
+    fill(context) {
+        context.save();
+        this.createPath(context);
+        context.fillStyle = this.fillStyle;
+        context.fill();
+        context.restore();
+    }
+    move(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 }
-function drawPolygon(loc) {
-        let {height, width} = rubberbandRect;
-        radius = Math.sqrt(height * height + width * width);
-    createPolygonPath(
+
+function drawPolygon() {
+    let {height, width} = rubberbandRect;
+    radius = Math.sqrt(height * height + width * width);
+    var polygon = new Polygon(
         mousedown.x,
         mousedown.y,
-        // rubberbandRect.width,
         radius,
         parseInt(sidesSelect.value),
-        (Math.PI / 180) * parseInt(startAngleSelect.value)
-    );
-    context.stroke();
-    if (fillCheckbox.checked) {
-        context.fill();
+        (Math.PI / 180) * parseInt(startAngleSelect.value),
+        context.strokeStyle,
+        context.fillStyle,
+        fillCheckbox.checked);
+
+    drawOnePolygon(polygon);
+
+    if (!dragging) {
+        polygons.push(polygon);
     }
-} 
+}
+
+function drawOnePolygon(polygon) {
+    context.beginPath();
+    polygon.createPath(context);
+    polygon.stroke(context);
+    if (fillCheckbox.checked) {
+        polygon.fill(context);
+    }
+}
+
+function drawStoredPolygons() {
+    polygons.forEach( function (polygon) {
+        drawPolygon(polygon);
+    });
+}
+
+// Dragging...........................................................
+function startDragging(loc) {
+    saveDrawingSurface();
+    mousedown.x = loc.x;
+    mousedown.y = loc.y;
+}
+function startEditing() {
+    canvas.style.cursor = 'pointer';
+    editing = true;
+}
+function stopEditing() {
+    canvas.style.cursor = 'crosshair';
+    editing = false;
+}
+
 // Guidewires.........................................................
 function drawHorizontalLine (y) {
     context.beginPath();
@@ -168,6 +232,8 @@ function drawGuidewires(x, y) {
     context.restore();
 }
 // Canvas event handlers..............................................
+// 画圆和直线
+/*
 canvas.onmousedown = function (e) {
     let loc = windowToCanvas(e.clientX, e.clientY);
     e.preventDefault(); // Prevent cursor change
@@ -175,6 +241,7 @@ canvas.onmousedown = function (e) {
     mousedown.x = loc.x;
     mousedown.y = loc.y;
     dragging = true;
+
 };
 canvas.onmousemove = function (e) {
     let loc;
@@ -194,6 +261,59 @@ canvas.onmouseup = function (e) {
     updateRubberband(loc);
     dragging = false;
 };
+*/
+
+canvas.onmousedown = function (e) {
+    var loc = windowToCanvas(e.clientX, e.clientY);
+    e.preventDefault(); // Prevent cursor change
+    if (editing) {
+        polygons.forEach( function (polygon) {
+            polygon.createPath(context);
+            if (context.isPointInPath(loc.x, loc.y)) {
+                startDragging(loc);
+                dragging = polygon;
+                draggingOffsetX = loc.x - polygon.x;
+                draggingOffsetY = loc.y - polygon.y;
+                return;
+            }
+        });
+    }
+    else {
+        startDragging(loc);
+        dragging = true;
+    }
+};
+canvas.onmousemove = function (e) {
+    var loc = windowToCanvas(e.clientX, e.clientY);
+    e.preventDefault(); // Prevent selections
+    if (editing && dragging) {
+        dragging.x = loc.x - draggingOffsetX;
+        dragging.y = loc.y - draggingOffsetY;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawGrid('lightgray', 10, 10);
+        drawStoredPolygons();
+    }
+    else {
+        if (dragging) {
+            restoreDrawingSurface();
+            updateRubberband(loc);
+            if (guidewires) {
+                drawGuidewires(mousedown.x, mousedown.y);
+            }
+        }
+    }
+};
+
+canvas.onmouseup = function (e) {
+    var loc = windowToCanvas(e.clientX, e.clientY);
+    dragging = false;
+    if (editing) {
+    }
+    else {
+        restoreDrawingSurface();
+        updateRubberband(loc);
+    }
+};
 // Controls event handlers.......................................
 eraseAllButton.onclick = function (e) {
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -203,10 +323,28 @@ eraseAllButton.onclick = function (e) {
 strokeStyleSelect.onchange = function (e) {
     context.fillStyle = context.strokeStyle = strokeStyleSelect.value;
 };
+/*
+fillStyleSelect.onchange = function (e) {
+    context.fillStyle = fillStyleSelect.value;
+};
+*/
+editCheckbox.onchange = function (e) {
+    if (editCheckbox.checked) {
+        startEditing();
+    }
+    else {
+        stopEditing();
+    }
+};
 guidewireCheckbox.onchange = function (e) {
     guidewires = guidewireCheckbox.checked;
 };
 // Initialization................................................
 context.strokeStyle = strokeStyleSelect.value;
+// context.fillStyle = fillStyleSelect.value;
+context.shadowColor = 'rgba(0,0,0,0.4)';
+context.shadowOffsetX = 2;
+context.shadowOffsetY = 2;
+context.shadowBlur = 4;
 drawGrid('lightgray', 10, 10);
 
