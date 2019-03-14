9bbfe93f5f55fb166fa1a2db0678d1323e9fc739
boom <5642896557@qq.com>
2019-03-14 08:21:13 +0800

    添加辅助点函数

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -78,8 +78,45 @@ export default {
     mounted() {
         this.getContext();
         this.drawRubberbandLines();
+        this.drawCheckbox();
     },
     methods: {
+        drawCheckbox() {
+            let { context } = this;
+            context.fillStyle = 'cornflowerblue';
+            context.strokeStyle = 'yellow';
+            context.shadowColor = 'rgba(50,50,50,1.0)';
+            context.shadowOffsetX = 2;
+            context.shadowOffsetY = 2;
+            context.shadowBlur = 4;
+            context.lineWidth = 20;
+            context.lineCap = 'round';
+            context.beginPath();
+            context.moveTo(120.5, 130);
+            context.quadraticCurveTo(150.8, 130, 160.6, 150.5);
+            context.quadraticCurveTo(190, 250.0, 210.5, 160.5);
+            context.quadraticCurveTo(240, 100.5, 290, 70.5);
+            context.stroke();
+            let points = [120.5, 130, 150.8, 130, 160.6, 150.5, 190, 250.0, 210.5, 160.5, 240, 100.5, 290, 70.5];
+            this.drawBatchPoint({ points });
+        },
+        drawBatchPoint({ points }) {
+            points.forEach((x, index) => {
+                if ((index < points.length - 1) && (index + 1) % 2 !== 0) {
+                    let y = points[index + 1];
+                    this.drawPoint({ x, y })
+                }
+            });
+        },
+        drawPoint({ x, y, radius = 2 }) {
+            let { context } = this;
+            context.save();
+            context.beginPath();
+            context.fillStyle = 'red';
+            context.arc(x, y, radius, 0, 2 * Math.PI, false);
+            context.fill();
+            context.restore();
+        },
         getContext() {
             this.canvas = document.getElementById('canvas');
             this.context = this.canvas.getContext('2d');
