a41bf50f7dc82445130f415814cc4d2dffcf5dcf
boom <564296557@qq.com>
2019-05-08 14:39:31 +0800

    [alter] camera随着mario的移动而移动

--- src/views/superMario/index.vue
+++ src/views/superMario/index.vue
@@ -64,8 +64,12 @@
                 setupMouseControl(canvas, mario, camera);
                 const timer = new Timer(1/60);
                 timer.update = function update(deltaTime) {
-                    // mario.update(deltaTime);
                     level.update(deltaTime);
+
+                    if (mario.pos.x > 100) {
+                        camera.pos.x = mario.pos.x - 100;
+                    }
+
                     level.comp.draw(ctx, camera);
                 };
                 timer.start();
