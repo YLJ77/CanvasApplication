4a14de28fb040510a2c1b2cbb68ed706c7275479
boom <564296557@qq.com>
2019-05-08 14:54:44 +0800

    [add] 添加加速减速状态

--- src/views/superMario/traits/Go.js
+++ src/views/superMario/traits/Go.js
@@ -5,18 +5,26 @@ export default class Go extends Trait{
         super('go');
 
         this.dir = 0;
-        this.speed = 12000;
+        this.acceleration = 800;
+        this.deceleration = 300;
+        this.dragFactor = 1/5000;
         this.distance = 0;
         this.heading = 1;
     }
 
     update(entity, deltaTime) {
-        entity.vel.x = this.speed * this.dir * deltaTime;
+        const absX = Math.abs(entity.vel.x);
         if (this.dir) {
+            entity.vel.x += this.acceleration * this.dir * deltaTime;
             this.heading = this.dir;
-            this.distance += Math.abs(entity.vel.x) * deltaTime;
+            this.distance += absX * deltaTime;
+        } else if (entity.vel.x !== 0) {
+            const decel = Math.min(absX, this.deceleration * deltaTime);
+            entity.vel.x += entity.vel.x > 0 ? -decel : decel;
         } else {
             this.distance = 0;
         }
+        const drag = this.dragFactor * entity.vel.x * absX;
+        entity.vel.x -= drag;
     }
 }
