3b4687819361162f741793295bd727f3a042306e
boom <564296557@qq.com>
2019-05-09 14:15:46 +0800

    [add] 添加背景pe

--- src/views/superMario/loaders.js
+++ src/views/superMario/loaders.js
@@ -13,36 +13,6 @@ export function loadImage(src) {
     });
 }
 
-function createTiles(level, backgrounds) {
-    function applyRange(background, xStart, xLen, yStart, yLen) {
-        const xEnd = xStart + xLen;
-        const yEnd = yStart + yLen;
-
-        for(let x=xStart; x<xEnd; ++x) {
-            for (let y=yStart; y<yEnd; ++y) {
-                level.tiles.set(x, y, {
-                    name: background.tile,
-                    type: background.type
-                });
-            }
-        }
-    }
-
-    backgrounds.forEach(background => {
-        background.ranges.forEach(range => {
-            if (range.length === 4) {
-                const [xStart, xLen, yStart, yLen] = range;
-                applyRange(background, xStart, xLen, yStart, yLen)
-            } else if (range.length === 3){
-                const [xStart, xLen, yStart] = range;
-                applyRange(background, xStart, xLen, yStart, 1);
-            } else if (range.length === 2) {
-                const [xStart, yStart] = range;
-                applyRange(background, xStart, 1, yStart, 1);
-            }
-        })
-    });
-}
 
 async function loadJson(url) {
     return new Promise(resolve => {
@@ -86,14 +56,52 @@ export async function loadSpriteSheet(name) {
     return sprites;
 }
 
-export async function loadLevel(name, ctx) {
+export async function loadLevel(name) {
     let levelSpec = await loadJson(`levels/${name}.json`)
     const level = new Level();
-    createTiles(level, levelSpec.backgrounds);
+    createTiles(level, levelSpec.backgrounds, levelSpec.patterns);
     let backgroundSprite = await loadSpriteSheet(levelSpec.spriteSheet);
     const backgroundLayer = createBackgroundLayer(level, backgroundSprite);
     level.comp.layers.push(backgroundLayer);
     const spriteLayer = createSpriteLayer(level.entities);
     level.comp.layers.push(spriteLayer);
     return level;
+}
+
+function createTiles(level, backgrounds, patterns, offsetX = 0, offsetY = 0) {
+    function applyRange(background, xStart, xLen, yStart, yLen) {
+        const xEnd = xStart + xLen;
+        const yEnd = yStart + yLen;
+
+        for(let x=xStart; x<xEnd; ++x) {
+            for (let y=yStart; y<yEnd; ++y) {
+                const derivedX = x + offsetX;
+                const derivedY = y + offsetY;
+                if (background.pattern) {
+                    const backgrounds = patterns[background.pattern].backgrounds;
+                    createTiles(level, backgrounds, patterns, derivedX, derivedY);
+                } else {
+                    level.tiles.set(derivedX, derivedY, {
+                        name: background.tile,
+                        type: background.type
+                    });
+                }
+            }
+        }
+    }
+
+    backgrounds.forEach(background => {
+        background.ranges.forEach(range => {
+            if (range.length === 4) {
+                const [xStart, xLen, yStart, yLen] = range;
+                applyRange(background, xStart, xLen, yStart, yLen)
+            } else if (range.length === 3){
+                const [xStart, xLen, yStart] = range;
+                applyRange(background, xStart, xLen, yStart, 1);
+            } else if (range.length === 2) {
+                const [xStart, yStart] = range;
+                applyRange(background, xStart, 1, yStart, 1);
+            }
+        })
+    });
 }
